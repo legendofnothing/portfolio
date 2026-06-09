ad418b646ba6103aef372dc8ad92cab308ba56d0
legendofnothing <huyrtb123@gmail.com>
2026-06-09 22:44:25 +0700

    d

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log(':3')
+const container = document.getElementById('canvas-container');
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(5
+    , window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+container.appendChild(renderer.domElement);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
+
+let model;
+const mtlLoader = new THREE.MTLLoader();
+
+mtlLoader.load('model.mtl', function (materials) {
+    materials.preload();
+
+    const objLoader = new THREE.OBJLoader();
+    objLoader.setMaterials(materials);
+
+    objLoader.load('model.obj', function (obj) {
+        model = obj;
+        scene.add(model);
+    }, undefined, function (error) {
+        console.error(error);
+    });
+}, undefined, function (error) {
+    console.error(error);
+});
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    if (model) {
+        model.rotation.x = 0.5;
+        model.rotation.y += 0.01;
+    }
+
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
